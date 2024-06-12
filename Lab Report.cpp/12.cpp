eb1ee4c88fe67838c7e69040fd5d991d7cdcb2e5
ashik-maz <ashik.cse.pust@gmail.com>
2024-06-12 13:28:21 +0600

    Stack

--- Lab Report.cpp/12.cpp
+++ Lab Report.cpp/12.cpp
@@ -1,7 +1,46 @@
-//#include<bits/stdtrc1c++.h>
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+template <typename Type>
+class Stack{
+    int MAX_SIZE=1e6;
+    int last=-1;
+    Type *arr =new Type[MAX_SIZE];
+
+    public:
+    void push(Type value){
+        ++last;
+        arr[last]=value;
+    }
+    void pop(){
+        --last;
+    }
+    Type top(){
+        return arr[last];
+    }
+    int size(){
+        return last+1;
+    }
+
+    bool empty(){
+        if(last==-1) return true;
+        return false;
+    }
+};
+
 int main(){
-    cout<<"Hello World"<<endl;
+    Stack <int> st;
+    st.push(1);
+    st.push(19);
+    st.push(13);
+    st.push(32);
+    st.push(33);
+    st.pop();
+    cout<<"size of Stack= "<<st.size()<<"\n";
+
+    cout<<"element Are: ";
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
 }
